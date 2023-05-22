a5816df869c82223682f1972f94efa54a9ec3cde
akiko_kuroda <hahnecochito@gmail.com>
2023-05-23 01:46:55 +0900

    さまざまなプロパティの渡し方

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,12 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
       <div className="container">
-        <Headline page="index" />
+        <Headline
+          page="index"
+          number={1111}
+          array={[1, 2, 3]}
+          obj={{ foo: "foo", bar: "bar" }}
+          boolean />
       </div>
     </main>
   )
