64a6e159eeeae3ed7ad4d83d6682159f41d9198c
akiko_kuroda <hahnecochito@gmail.com>
2023-05-23 01:38:24 +0900

    fix

--- src/app/components/Headline.tsx
+++ src/app/components/Headline.tsx
@@ -13,7 +13,7 @@ export default function Headline(props) {
 
       <h1 className="h1 title">{props.page} page</h1>
       <p className="my-3 text-primary text-center">Get started by editing{" "}</p>
-      <p className="my-3 text-center"><small>pages/{props.page}.js</small></p>
+      <p className="my-3 text-center"><span>pages/{props.page}.js</span></p>
     </div>
   )
 }
