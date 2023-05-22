7484eb87c60fe34a35be6bc3515d44beb0a8e72f
akiko_kuroda <hahnecochito@gmail.com>
2023-05-23 00:23:25 +0900

    修正

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,22 @@
+'use client'
+import 'bootstrap/dist/css/bootstrap.css'
+import styles from '../styles/Home.module/css'
+
 const About = () => {
-  return <p>Aboutページ</p>
+  return (
+    <>
+      <style jsx>
+        {`
+          p {
+            text-align: center;
+            color: white;
+            font-size: 18pt;
+          }
+        `}
+      </style>
+      <p className="h5 bg-primary">Aboutページ</p>
+    </>
+  )
 }
 
 export default About;
