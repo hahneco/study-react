cac373b7180d9f3a21a21ce2046f3b0438867c75
akiko_kuroda <hahnecochito@gmail.com>
2023-05-24 00:10:22 +0900

    fix

--- src/app/components/Main/index.tsx
+++ src/app/components/Main/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap/dist/css/bootstrap.css'
+import '../../globals.css'
 import Headline from '../Headline';
 
 const Main = (props) => {
@@ -13,6 +14,25 @@ const Main = (props) => {
         >
           code={<p className="my-3 text-center"><span>pages/{props.page}.js</span></p>}
         </Headline>
+      </div>
+
+      <div className="container">
+        <div className="px-6 py-4 grid-rows-4">
+          <h3 className='font-bold text-xl mb-2'>title</h3>
+          <p className='text-gray-700 text-base'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+        </div>
+        <div className="px-6 py-4 grid-rows-4">
+          <h3 className='font-bold text-xl mb-2'>title</h3>
+          <p className='text-gray-700 text-base'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+        </div>
+        <div className="px-6 py-4 grid-rows-4">
+          <h3 className='font-bold text-xl mb-2'>title</h3>
+          <p className='text-gray-700 text-base'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+        </div>
+        <div className="px-6 py-4 grid-rows-4">
+          <h3 className='font-bold text-xl mb-2'>title</h3>
+          <p className='text-gray-700 text-base'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+        </div>
       </div>
     </main>
   )
